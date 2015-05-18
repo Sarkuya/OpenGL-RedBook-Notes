a269bccc4957b56c0a6a929e692eea891a8e5889
Sarkuya <sarkuya@hotmail.com>
2015-05-18 17:23:01 +0800

    Generats single and chunk html files for DocBook.

--- Docs/DocBook/GenHTML.sh
+++ Docs/DocBook/GenHTML.sh
@@ -1,5 +1,27 @@
-rm -r Dist
-mkdir -p Dist/{css,images}
-cp CSS/style.css Dist/css
+export DOCBOOK_XSL_DIR="/usr/share/sgml/docbook/xsl-stylesheets"
 
-xsltproc --xinclude --output Dist/SampleBook.html XSL/html.xsl Content/SampleBook.xml
+rm -r Dist
+
+mkdir -p Dist/html/chunk/css
+mkdir -p Dist/html/chunk/images/custom
+mkdir -p Dist/html/chunk/images/docbook
+
+mkdir -p Dist/html/single/css
+mkdir -p Dist/html/single/images/custom
+mkdir -p Dist/html/single/images/docbook
+
+mkdir -p Dist/pdf/images/custom
+mkdir -p Dist/pdf/images/docbook
+
+cp -r CSS/* Dist/html/chunk/css
+cp -r CSS/* Dist/html/single/css
+
+cp -r IMAGES/* Dist/html/chunk/images/custom
+cp -r $DOCBOOK_XSL_DIR/images/* Dist/html/chunk/images/docbook
+cp -r IMAGES/* Dist/html/single/images/custom
+cp -r $DOCBOOK_XSL_DIR/images/* Dist/html/single/images/docbook
+cp -r IMAGES/* Dist/pdf/images/custom
+cp -r $DOCBOOK_XSL_DIR/images/* Dist/pdf/images/docbook
+
+xsltproc --xinclude --output Dist/html/single/SampleBook.html XSL/html.xsl Content/SampleBook.xml
+xsltproc --xinclude --output Dist/html/chunk/SampleBook.html XSL/chunk.xsl Content/SampleBook.xml
--- Docs/DocBook/XSL/chunk.xsl
+++ Docs/DocBook/XSL/chunk.xsl
@@ -1,10 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
 
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-  <xsl:import href="/usr/share/sgml/docbook/xsl-stylesheets/html/docbook.xsl" />
+  <xsl:import href="/usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl" />
   <xsl:param name="html.stylesheet" select="'css/style.css'" />
 
-  <xsl:output method="html" encoding="UTF-8" indent="yes" />
+  <xsl:param name="chunker.output.encoding" select="'UTF-8'" />
+  <xsl:param name="chunker.output.indent" select="'yes'" />
 
   <xsl:param name="toc.section.depth" select="2" />
 
@@ -15,4 +16,8 @@
   <xsl:param name="admon.graphics.path" select="'images/docbook/'" />
   <xsl:param name="admon.graphics.extension" select="'.png'" />
   <xsl:param name="admon.textlabel" select="0" />
+
+  <xsl:param name="navig.graphics" select="1" />
+  <xsl:param name="navig.graphics.path" select="'images/docbook/'" />
+
 </xsl:stylesheet>
