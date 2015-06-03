cc49fb82c005543e58a78fbe35143650a5f16df6
Sarkuya <sarkuya@hotmail.com>
2015-06-03 17:13:46 +0800

    Update ConvertOpenGLManual.sh.

--- etc/utils/convertglmanual/ConvertOpenGLManual.sh
+++ etc/utils/convertglmanual/ConvertOpenGLManual.sh
@@ -1,5 +1,17 @@
-xsltproc --output Dist/glGenBuffers.xml localstyle.xsl glmansource/glGenBuffers.xml
-xsltproc --output Dist/glBindBuffer.xml localstyle.xsl glmansource/glBindBuffer.xml
-xsltproc --output Dist/glDeleteBuffers.xml localstyle.xsl glmansource/glDeleteBuffers.xml
+
+rm -r ./Dist
+
+declare -a SourceFiles=(glBindBuffer.xml glDeleteBuffers.xml glGenBuffers.xml)
+echo ${SourceFiles[0]}
+echo ${#SourceFiles[@]}
+
+for f in ${SourceFiles[@]}
+do
+echo $f
+done
+
+xsltproc --output ./Dist/ localstyle.xsl glmansource/glGenBuffers.xml
+xsltproc --output ./Dist/ localstyle.xsl glmansource/glBindBuffer.xml
+xsltproc --output ./Dist/ localstyle.xsl glmansource/glDeleteBuffers.xml
 
 
