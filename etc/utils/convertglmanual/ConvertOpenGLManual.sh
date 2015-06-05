da86ece1d3fb64d90e7f78f62ec23606bfafae75
Sarkuya <sarkuya@hotmail.com>
2015-06-05 10:20:23 +0800

    Convert files in local directory in utils.

--- etc/utils/convertglmanual/ConvertOpenGLManual.sh
+++ etc/utils/convertglmanual/ConvertOpenGLManual.sh
@@ -1,26 +1,27 @@
 ﻿
 XSL_FILE="localstyle.xsl"
 SOURCE_DIR="glmansource"
+SOURCE_FILES=()
 DIST_DIR="Dist"
+INCLUDED_DIR="included"
 
 rm -r $DIST_DIR
 
-mkdir -p Dist/included
-cp -r glmansource/included Dist
+mkdir -p $DIST_DIR/$INCLUDED_DIR
+cp -r $SOURCE_DIR/$INCLUDED_DIR $DIST_DIR
 
-declare -a SourceFiles=()
-
-# echo ${SourceFiles[0]}
-# echo ${#SourceFiles[@]}
+# echo ${SOURCE_FILES[0]}
+# echo ${#SOURCE_FILES[@]}
 
 for f in $SOURCE_DIR/*.xml
 do
-FILE="${f/'glmansource/'/}"
-SourceFiles=("${SourceFiles[@]}" $FILE)
+FILE="${f/$SOURCE_DIR\//}"
+SOURCE_FILES=("${SOURCE_FILES[@]}" $FILE)
 done
 
-for f in ${SourceFiles[@]}
+for f in ${SOURCE_FILES[@]}
 do
-echo $f
 xsltproc --output $DIST_DIR/$f $XSL_FILE $SOURCE_DIR/$f
 done
+
+
